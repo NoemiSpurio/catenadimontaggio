9e987a31e10568d970eeb1b770dbffe11be89e4d
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-11 16:37:37 +0100

    modifica al mock

--- src/main/java/it/prova/catenadimontaggio/dao/DbMock.java
+++ src/main/java/it/prova/catenadimontaggio/dao/DbMock.java
@@ -15,18 +15,24 @@ public class DbMock {
 		SlotCatenaDiMontaggio slot1 = new SlotCatenaDiMontaggio(1L, "Toyota", "Giappone", new ArrayList<Automobile>());
 		Automobile toyota1 = new Automobile(1L, "Yaris", "Blu", new Date());
 		slot1.getAutomobili().add(toyota1);
+		toyota1.setCatenaDiMontaggio(slot1);
 		Automobile toyota2 = new Automobile(2L, "Aygo", "Rosso", new Date());
 		slot1.getAutomobili().add(toyota2);
+		toyota2.setCatenaDiMontaggio(slot1);
 		SlotCatenaDiMontaggio slot2 = new SlotCatenaDiMontaggio(2L, "Honda", "Giappone", new ArrayList<Automobile>());
 		Automobile honda1 = new Automobile(3L, "Civic", "Nero", new Date());
 		slot2.getAutomobili().add(honda1);
+		honda1.setCatenaDiMontaggio(slot2);
 		Automobile honda2 = new Automobile(4L, "Jazz", "Arancio", new Date());
 		slot2.getAutomobili().add(honda2);
+		honda2.setCatenaDiMontaggio(slot2);
 		SlotCatenaDiMontaggio slot3 = new SlotCatenaDiMontaggio(3L, "Fiat", "Italia", new ArrayList<Automobile>());
 		Automobile fiat1 = new Automobile(5L, "Panda", "Bianco", new Date());
 		slot3.getAutomobili().add(fiat1);
+		fiat1.setCatenaDiMontaggio(slot3);
 		Automobile fiat2 = new Automobile(6L, "Pandino", "Rosa", new Date());
 		slot3.getAutomobili().add(fiat2);
+		fiat2.setCatenaDiMontaggio(slot3);
 
 		SLOTS_CATENA_MONTAGGIO.add(slot1);
 		SLOTS_CATENA_MONTAGGIO.add(slot2);
